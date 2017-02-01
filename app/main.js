472a4ba8ef0f42dab3fc638f8399974744b6265d
ralphwillgoss <ralph.willgoss@gmail.com>
2017-02-01 22:36:00 +0000

    Add AutoUpdate functionality

--- app/main.js
+++ app/main.js
@@ -33,7 +33,14 @@ function createWindow () {
   })
   
   autoUpdater.setFeedURL("http://localhost/workbench/")
-  autoUpdater.checkForUpdates()
+  autoUpdater.on('checking-for-update', () => console.log("checking for update"))
+  autoUpdater.on('update-available', () => console.log("update available"))
+  autoUpdater.on('update-not-available', () => console.log("update not available"))
+  autoUpdater.on('update-downloaded', () => {
+    console.log("update downloaded")
+    autoUpdater.quitAndInstall()
+  })
+  autoUpdater.checkForUpdates();
 
   require('./main-menu')
 }
